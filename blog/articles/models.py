d1475903fac03ab7c60e5df5be561e11cf57c768
Vinícius Bertoldi <bertoldi11@gmail.com>
2015-07-15 19:00:26 -0300

    Configuring admin area

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField(max_length=200)
     text = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return u'%s' % self.title
